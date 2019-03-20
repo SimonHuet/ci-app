1ef960fea78e9c8b469a775eea17d9db19eab173
unknown <simon.huet@epsi.fr>
2019-03-20 11:31:28 +0100

    circle-ci

--- src/components/list-facts/fact/fact.js
+++ src/components/list-facts/fact/fact.js
@@ -4,7 +4,7 @@ export const Fact = ({
   fact = {}
 }) =>
   <li className="list-group-item">
-      <span style={{float:"left", marginRight :"25px"}} className="badge badge-pill">
+      <span style={{float:"left", marginRight :"25px" , postion :"absolute"}} className="badge badge-pill">
       <img alt='fact_icon' src={fact.icon_url} /></span>
       <blockquote >{ fact.value }</blockquote>
   </li>
